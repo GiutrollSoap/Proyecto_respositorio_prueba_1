fbf8c55d369f0e75fa50523a42c9265712058721
SoapGiutroll <Giutroll117troll@gmail.com>
2025-06-23 12:04:35 -0600

    Creacion de clase visitor
    Se crea el paquete visitors y la clase visitor, se le agrega sus get,set,tostring y cstructor, tambien se verifica bien el numero de telefono

--- src/Visitors/Visitor.java
+++ src/Visitors/Visitor.java
@@ -4,10 +4,74 @@
  */
 package Visitors;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author UTN
  */
 public class Visitor {
+    private String Id;
+    private String Completename;
+    private LocalDate Birthdate;
+    private String Celphone; 
+
+    public Visitor(String Id, String Completename, LocalDate Birthdate, String Celphone) {
+        this.Id = Id;
+        this.Completename = Completename;
+        this.Birthdate = Birthdate;
+        this.Celphone = null;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public void setCompletename(String Completename) {
+        this.Completename = Completename;
+    }
+
+    public void setBirthdate(LocalDate Birthdate) {
+        this.Birthdate = Birthdate;
+    }
+
+    public void setCelphone(String Celphone) {
+               if(Celphone.matches("\\d{2}-\\d{2}-\\d{2}-\\d{2}")){
+        this.Celphone = Celphone;
+    }
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public String getCompletename() {
+        return Completename;
+    }
+
+    public LocalDate getBirthdate() {
+        return Birthdate;
+    }
+
+    public String getCelphone() {
+        return Celphone;
+    }
+
+    @Override
+    public String toString() {
+        return "Visitor" + "Id=" + Id + ", Completename=" + Completename + ", Birthdate=" + Birthdate + ", Celphone=" + Celphone;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
