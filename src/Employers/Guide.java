5099213334b68c4eaba196bc9cd019b508ff9712
SoapGiutroll <Giutroll117troll@gmail.com>
2025-06-23 11:48:43 -0600

    creacion de classe Guide
    Se crea la clase guide con su informacion, su gst,set,To string, costructor y las variables para los Datos espesificados

--- src/Employers/Guide.java
+++ src/Employers/Guide.java
@@ -4,10 +4,56 @@
  */
 package Employers;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author UTN
  */
-public class Guide {
+public class Guide extends Employeer{
+    private String Languages;
+    private String Tours;
+
+    public Guide(String Languages, String Tours, int Id, String CompleteName, LocalDate BirthDate, String CelphoneNumber, int Salary) {
+        super(Id, CompleteName, BirthDate, CelphoneNumber, Salary);
+        this.Languages = Languages;
+        this.Tours = "";
+    }
+    
+    public void addLanguages(String Languages){
+           if (!Languages.toLowerCase().contains(Languages.toLowerCase())) {
+            if (!Languages.isEmpty()) {
+                Languages += "\n";
+            }
+            Languages += Languages;
+        }
+    }
+
+    public void registerTour(String description) {
+        if (!Tours.isEmpty()) {
+            Tours += "\n";
+        }
+        Tours += description;
+    }
+    
+    
+    public String getLanguages() {
+        return Languages;
+    }
+
+    public String getTours() {
+        return Tours;
+    }
+    
+  
+    public String getType(){
+        return "Guide";
+    }
+
+    @Override
+    public String toString() {
+        return "Guide{" + "Languages=" + Languages + ", Tours=" + Tours ;
+    }
+    
     
 }
