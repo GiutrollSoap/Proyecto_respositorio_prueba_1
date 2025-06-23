08a5f462cc1190cac01497a763554fb2b422139d
SoapGiutroll <Giutroll117troll@gmail.com>
2025-06-23 11:27:36 -0600

    Se crea keeper
    Se creo la clase keeper con sus costructor, set, get y string

--- src/Employers/Keeper.java
+++ src/Employers/Keeper.java
@@ -4,10 +4,46 @@
  */
 package Employers;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author UTN
  */
-public class Keeper {
+public class Keeper extends Employeer {
     
+    
+    private String Activity;
+    private String Especiality;
+
+    public Keeper(String Activity, String Especiality, int Id, String CompleteName, LocalDate BirthDate, String CelphoneNumber, int Salary) {
+        super(Id, CompleteName, BirthDate, CelphoneNumber, Salary);
+        this.Activity = Activity;
+        this.Especiality = Especiality;
+    }
+
+    public String getActivity() {
+        return Activity;
+    }
+
+    public String getEspeciality() {
+        return Especiality;
+    }
+
+    
+   public String getType(){
+       return "Keeper";
+   }
+   
+   public void DoActivitys (String Activity){
+         if (!Activity.isEmpty()) {
+            Activity += "\n";
+        }
+        Activity += Activity;  
+   }
+   
+    @Override
+    public String toString() {
+        return "Keeper{" + "Activity=" + Activity + ", Especiality=" + Especiality ;
+    }
 }
